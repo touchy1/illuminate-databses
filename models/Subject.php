8ad75181e6709b593f28e36c282c211170062337
touchy1 <touchymoonshakil1@gmail.com>
2018-04-06 22:12:37 +0600

    add subjects

---
+++ models/Subject.php
@@ -0,0 +1,9 @@
+<?php
+use Illuminate\Database\Eloquent\Model;
+
+class Subject extends Model{
+    protected $guarded=[];
+    public function department () {
+        return $this->belongTo(Department::class);
+    }
+}
